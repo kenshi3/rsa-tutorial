002d74107536d1c8c5e00b44a3298712906b8081
Kenshi Lau <ryukenlau@gmail.com>
2019-12-12 14:41:09 +0700

    update

--- index.js
+++ index.js
@@ -64,8 +64,6 @@ getKey = () => {
         public = 'Public Key => {' + e + ',' + f + '}'
         private = 'Private Key => {' + d + ',' + f + '}'
     }
-    // document.getElementById('p').setAttribute('class', 'inputnotext')
-    // document.getElementById('p').className = 'validate invalid'
     document.getElementById('ed').innerHTML = ed
     document.getElementById('public').innerHTML = public
     document.getElementById('private').innerHTML = private
@@ -86,7 +84,7 @@ encrypt = () => {
         cipher.push(String.fromCharCode(cipherASCII[i]))
     }
 
-    document.getElementById('ec').innerHTML = 'Cipher = Plain^' + e + ' mod ' + f
+    document.getElementById('ec').innerHTML = 'Cipher = Plain<sup>' + e + '</sup> mod ' + f
 
     document.getElementById('plainASCII').innerHTML = plainASCII.join('')
     document.getElementById('cipherASCII').innerHTML = cipherASCII.join('')
@@ -106,7 +104,7 @@ decrypt = cipherASCII => {
         plain_decrypt.push(String.fromCharCode(plainASCII_decrypt[i]))
     }
 
-    document.getElementById('ep').innerHTML = 'Plain = Cipher^' + d + ' mod ' + f
+    document.getElementById('ep').innerHTML = 'Plain = Cipher<sup>' + d + '</sup> mod ' + f
 
     document.getElementById('cipherASCII2').innerHTML = cipherASCII.join('')
     document.getElementById('plainASCII_decrypt').innerHTML = plainASCII_decrypt.join('')
